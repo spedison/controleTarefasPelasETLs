acc516c71f694a4bfe0478d081cc078d03e275f7
spedison <spedison@gmail.com>
2023-08-12 21:34:37 -0300

    Acessando o ElasticSearch. Falta testes mais gerais e fechamento da primeira versão do App. Testes mais intensos com Tarefas e Atividades. Ajustes nos scripts para facilitar a chamada Script de exemplo para utilizar na ETL (testeGeral.sh) Listagem de Tarefas e a evolução das atividades de cadas uma.
    >> Falta
    -- Fechar de modo forçado;
    -- Registro de Log caso não consiga começar a execução.

--- scripts/bash/comum.sh
+++ scripts/bash/comum.sh
@@ -1,5 +1,18 @@
 export JAVA_HOME=/home/spedison/.sdkman/candidates/java/17.0.7-zulu/
 export BASE_PROJECT=/mnt/dados/git/controleTarefaScripts/build/libs
+#Perfis usandos na aplicacao
 export spring_profiles_active=oracle,elastic-prod
+#Dados do Elastic
+export ELASTIC_USER=elastic
 export ELASTIC_PASS=123456
+export ELASTIC_HOST=elasticdb-7.casa.com.br
+#Dados do Oracle
+export BANCO=xe
+export BANCO_HOST=oraclexe-w11.casa.com.br
+export BANCO_USER=ADMATENA
+export BANCO_PASS=112233
+
+## Para habilitar o DEBUG basta colocar DEBUG=1
 export DEBUG=
+
+[ -z "$NOME_TAREFA" ] && export NOME_TAREFA="Etl Diaria Atena"
